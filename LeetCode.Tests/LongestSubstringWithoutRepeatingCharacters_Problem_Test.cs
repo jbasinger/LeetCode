ae67287fe8f6232f5efcbc11c3fb2ce5fae35ed0
Justin Basinger <justin.m.basinger@gmail.com>
2021-02-06 18:33:21 -0500

    Finishing problem 3

---
+++ LeetCode.Tests/LongestSubstringWithoutRepeatingCharacters_Problem_Test.cs
@@ -0,0 +1,79 @@
+﻿using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using NUnit.Framework;
+using Shouldly;
+
+namespace LeetCode.Tests
+{
+    public class LongestSubstringWithoutRepeatingCharacters_Problem_Test
+    {
+        /*
+         * Edge cases
+         * - All the same character
+         * - the long string is at the end
+         * - the long string is first
+         * - the long string is in the middle
+         * - the whole string is the substring
+         * - the string is empty
+         * - the string is one character
+         */
+        
+        private LongestSubstringWithoutRepeatingCharacters_Problem _sut = new LongestSubstringWithoutRepeatingCharacters_Problem();
+        
+        [Test]
+        public void ShouldReturnOneIfAllTheSame()
+        {
+            var str = "bbbbb";
+            var res = _sut.LengthOfLongestSubstring(str);
+            res.ShouldBe(1);
+        }
+
+        [Test]
+        public void ShouldReturnZeroIfEmpty()
+        {
+            var res = _sut.LengthOfLongestSubstring("");
+            res.ShouldBe(0);
+        }
+        
+        [Test]
+        public void ShouldReturnLongStringAtTheEnd()
+        {
+            var str = "rywtrwqewtytyuiop";
+            var res = _sut.LengthOfLongestSubstring(str);
+            res.ShouldBe(6);
+        }
+
+        [Test]
+        public void ShouldReturnLongStringAtBeginning()
+        {
+            var str = "poiuytytweqwrtwyr";
+            var res = _sut.LengthOfLongestSubstring(str);
+            res.ShouldBe(6);
+        }
+        
+        [Test]
+        public void ShouldReturnLongStringInTheMiddle()
+        {
+            var str = "qweqrrplikfudjggtyr";
+            var res = _sut.LengthOfLongestSubstring(str);
+            res.ShouldBe(10);
+        }
+        
+        [Test]
+        public void ShouldReturnTheWholeThing()
+        {
+            var str = "qwertyuiop";
+            var res = _sut.LengthOfLongestSubstring(str);
+            res.ShouldBe(str.Length);
+        }
+
+        [Test]
+        public void ShouldReturnOneIfOneCharacter()
+        {
+            var str = "o";
+            var res = _sut.LengthOfLongestSubstring(str);
+            res.ShouldBe(str.Length);
+        }
+        
+    }
+}
---
+++ LeetCode/LongestSubstringWithoutRepeatingCharacters_Problem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace LeetCode
+{
+    public class LongestSubstringWithoutRepeatingCharacters_Problem
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if(s.Length == 0)
+                return 0;
+
+            if (s.Length == 1)
+                return 1;
+            
+            var maxLength = 1;
+            var curLength = 0;
+            var chars= new Dictionary<char, bool>(); 
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                chars.Clear();
+                var firstChar = s[i];
+                chars[firstChar] = true;
+                curLength = 1;
+                
+                for (var j = i+1; j < s.Length; j++)
+                {
+                    var currentChar = s[j];
+                    if (chars.ContainsKey(currentChar))
+                    {
+                        if (j == s.Length - 1)
+                        {
+                            return maxLength;
+                        }
+                        break;
+                    }
+                    
+                    chars[currentChar] = true;
+                    curLength++;
+                    if (maxLength <= curLength)
+                        maxLength = curLength;
+                    
+                    if (j == s.Length - 1)
+                    {
+                        return maxLength;
+                    }
+                    
+                }
+            }
+            
+            return maxLength;
+        }
+    }
+}
